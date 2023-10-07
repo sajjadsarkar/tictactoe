30f0a4705af4d467f68740369d48e0a641b8fffa
IAmNotGameDev <ms.sazzad.0000@gmail.com>
2023-10-07 13:04:06 +0600

    bkp

--- Assets/Scripts/SM.cs
+++ Assets/Scripts/SM.cs
@@ -18,6 +18,10 @@ public class SM : MonoBehaviour
     {
         SceneManager.LoadScene(2);
     }
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
     // Update is called once per frame
     void Update()
     {
