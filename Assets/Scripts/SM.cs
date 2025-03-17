f2aa90df20f18a3b071f67fc85b825095d4af1e8
Sajjad Hossain <ms.sazzad.0000@gmail.com>
2025-03-17 22:12:44 +0600

    sav

--- Assets/Scripts/SM.cs
+++ Assets/Scripts/SM.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneManager : MonoBehaviour
+public class sm : MonoBehaviour
 {
 
     // Load scene by name
